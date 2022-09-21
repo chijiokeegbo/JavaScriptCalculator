f4d6cb84ca63699684141234679d09c017363955
Chijioke <chijioke.egbo@gmail.com>
2022-09-21 01:43:43 +0100

    completed JS

--- script.js
+++ script.js
@@ -1,23 +1,87 @@
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-const ops = [ '/', '*', '-', '+', '='];
+const ops = [ '/', '*', '-', '+'];
+const ids = {
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  0: 'zero',
+  '/': 'divide',
+  '*': 'multiply', 
+  '-': 'subtract', 
+  '+': 'add'
+}
 
 class App extends React.Component {
   state = {
     lastPressed: undefined,
-    currentNum: '0',
-    calc: undefined,
+    calc: '0',
     operation: undefined
   }
 
   handleClick = (e) => {
 
-    const {calc, currentNum} = this.state;
+    const {calc, lastPressed} = this.state;
     const {innerText} = e.target;
 
-    this.setState({
-      currentNum: currentNum + innerText
-    });
+    /*this.setState({
+      calc: currentNum + innerText,
+      currentNum: currentNum + innerText,
+      lastPressed: innerText
+    }); */
 
+    switch(innerText){
+      case 'AC' : {
+        this.setState({
+          calc: '0'
+        });
+        break;
+      }
+      case '=': {
+        const evaluated = eval(calc);
+        this.setState({
+          calc: evaluated
+        });
+        break;
+      }
+      case '.' : {
+        const splitted = calc.split(/[\+\-\*\/]/);
+        const last = splitted.slice(-1)[0];
+
+        if(!last.includes('.')){
+          this.setState({
+            calc: calc + '.'
+          })
+        }
+        break;
+      }
+      default: {
+        let e = undefined;
+        if(ops.includes(innerText)){
+        if(ops.includes(lastPressed) && innerText !== '-'){
+          const lastNumId = calc.split('').reverse().findIndex(char => char !== ' ' 
+            && nums.includes(+char));
+          e = calc.slice(0, calc.length -lastNumId) + `${innerText}`
+        }else{
+          e =  `${calc} ${innerText }`
+        }
+      } else {
+          e = (calc === '0') ? innerText : (calc + innerText);
+      }
+      this.setState({
+            calc: e
+        });
+
+        this.setState({
+          lastPressed: innerText
+        })
+    }
+    
     /*
     const {currentNum, prevNum, operation} = this.state;
     const {innerText} = e.target;
@@ -84,33 +148,59 @@ class App extends React.Component {
     //this.setState({
     //  lastPressed: innerText
     //});
-*/
+            */
+      }
+  }
+    render(){
+      const {calc} = this.state;
+    return (
+            <div className="calculator"><div 
+                id="display" 
+                className="display">
+                {calc}
+              </div>
+                <div className="nums-container">
+                  <button 
+                  className=" big-h light-gray ac" 
+                  onClick={this.handleClick} 
+                  id="clear"
+                  >
+                  AC
+                  </button>
+                  {nums.map(num => (
+                    <button 
+                      className={`dark-gray ? ${num === 0 && 'big-h'}`} 
+                      key={num} onClick={this.handleClick}
+                      id={ids[num]}
+                    >{num}</button>
+                  ))}
+                  <button 
+                    className="light-gray" 
+                    onClick={this.handleClick} 
+                    id="decimal"
+                  >.</button>
+                </div>
+                <div className="ops-container">
+                  {ops.map(op =>(
+                    <button 
+                      id={ids[op]} 
+                      className={`orange ? ${op === '=' && 'big'}`} 
+                      key={op} 
+                      onClick={this.handleClick}
+                    >{op}</button>
+                  ))}
+                  <button 
+                    className="orange" 
+                    onClick={this.handleClick} 
+                    id="equals"
+                  >=</button>
+                </div>
+              
+            </div>
+          )
+      }
   }
 
-  render(){
-    const {currentNum, prevNum, operation} = this.state;
-  return (
-          <div className="calculator">
-            <p style={{position: 'absolute', top: '0'}}>{JSON.stringify(this.state, null, 2)}</p>
-            <div id="display" className="display">
-              <small>{calc} </small>
-              {currentNum}
-            </div>
-              <div className="nums-container">
-                <button className=" big-h light-gray ac" onClick={this.handleClick}>AC</button>
-                {nums.map(num => (
-                  <button className={`dark-gray ? ${num === 0 && 'big-h'}`} key={num} onClick={this.handleClick}>{num}</button>
-                ))}
-                <button className="light-gray" onClick={this.handleClick}>.</button>
-              </div>
-              <div className="ops-container">
-                {ops.map((op, idx) =>(
-                  <button id="equals" className={`orange ? ${op === '=' && 'big'}`} key={op} onClick={this.handleClick}>{op}</button>
-                ))}
-              </div>
-            
-          </div>
-        )
-    }
-  }
+
+  
 ReactDOM.render(<App/>, document.getElementById("root"));
